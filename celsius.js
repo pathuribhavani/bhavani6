ae7d4f5c6a701222fddf70f42044a5096d0696a3
pathuribhavani <bavani.pathuri@gmail.com>
2019-03-01 12:28:57 -0600

    chnages made

---
+++ celsius.js
@@ -0,0 +1,6 @@
+function multiplyBy()
+{
+        num1 = parseInt(document.getElementById("degree").value);
+        res1= num1 + 273.15;
+        document.getElementById("result").innerHTML = res1;
+}
